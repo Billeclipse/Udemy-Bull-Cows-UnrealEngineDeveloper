d4c819ceab99b770148b1e1b4868888e2375b507
Billeclipse <billchelseaaek@hotmail.com>
2019-04-24 07:26:08 +0300

    Lectures 64-69

--- Section_2/BullCowGame/FBullCowGame.cpp
+++ Section_2/BullCowGame/FBullCowGame.cpp
@@ -1,10 +1,32 @@
+#pragma once
+
 #include "FBullCowGame.h"
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <time.h>  // needed for the time function which is a parameter for the srand
 #include <map>
 #define TMap std::map
 
-FBullCowGame::FBullCowGame() { Reset(); }
+// default constructor
+FBullCowGame::FBullCowGame() {
+	srand(time(NULL));
+	// The constant words has to be an isogram also has to to be max length 9 letters long
+	HiddenWords[1] = "planet";
+	HiddenWords[2] = "dog";
+	HiddenWords[3] = "wolf";
+	HiddenWords[4] = "isogram";
+	HiddenWords[5] = "marvelous";
+	for (int32 i = 1; i < HiddenWords.size() + 1; i++) {
+		RandomNumbers.push_back(i);
+	}
+	std::iota(RandomNumbers.begin(), RandomNumbers.end(), 1);
+	std::random_shuffle(RandomNumbers.begin(), RandomNumbers.end());
 
-int32 FBullCowGame::GetMaxTries() const { return MyMaxTries; }
+	bMoreWords = true;
+
+	Reset();
+}
 
 int32 FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
 
@@ -12,13 +34,25 @@ int32 FBullCowGame::GetHiddenWordLength() const { return MyHiddenWord.length(); 
 
 bool FBullCowGame::IsGameWon() const { return bGameIsWon; }
 
-void FBullCowGame::Reset()
-{	
-	constexpr int32 MAX_TRIES = 8;
-	MyMaxTries = MAX_TRIES;
+bool FBullCowGame::HasMoreWords() const { return bMoreWords; }
 
-	const FString HIDDEN_WORD = "planet";
-	MyHiddenWord = HIDDEN_WORD;
+int32 FBullCowGame::GetMaxTries() const {
+	TMap<int32, int32> WordLengthToMaxTries{ {3,4}, {4,7}, {5,10}, {6,14}, {7,20}, {8,28}, {9,38} };
+	return WordLengthToMaxTries[GetHiddenWordLength()];
+}
+
+void FBullCowGame::Reset()
+{
+	if (!RandomNumbers.empty()) {
+		// selecting a random hidden word from our TMap content
+		MyHiddenWord = HiddenWords[RandomNumbers.back()];
+		// removing that number from the list so it doesn't choose it again
+		RandomNumbers.pop_back();	
+	}
+	else {
+		bMoreWords = false;
+	}
+
 	MyCurrentTry = 1;
 	bGameIsWon = false;
 }
@@ -28,7 +62,7 @@ EGuessStatus FBullCowGame::IsGuessValid(FString guess) const
 	if (!IsIsogram(guess)) { // if the guess isn't an isogram
 		return EGuessStatus::NOT_ISOGRAM;
 	}
-	else if (false) { // if the guess isn't all lowercase TODO write function
+	else if (!IsLowercase(guess)) { // if the guess isn't all lowercase TODO write function
 		return EGuessStatus::NOT_LOWERCASE;
 	}
 	else if (guess.length() != GetHiddenWordLength()) { // if the guesss length is wrong
@@ -50,7 +84,6 @@ FBullCowCount FBullCowGame::SubmitValidGuess(FString Guess)
 	for (int32 i = 0; i < GetHiddenWordLength(); i++) {
 		// compare letters against the guess word	
 		for (int32 j = 0; j < Guess.length(); j++) {
-			// if they match thenadfasdf
 			if (MyHiddenWord[i] == Guess[j]) {
 				// if they're in the same place	
 				if (i == j) {
@@ -85,6 +118,20 @@ bool FBullCowGame::IsIsogram(FString guess) const
 		else { // otherwise
 			LetterSeen[Letter] = true; //add the letter to the map as seen
 		}		
+	}
+
+	return true; // for example in cases where \O is entered
+}
+
+bool FBullCowGame::IsLowercase(FString guess) const
+{
+	// handle 0 letter words as lower
+	if (guess.length() < 1) { return true; }
+
+	for (auto letter : guess) { // for all letters of the guess
+		if (!islower(letter)) { // if the letter is in lowercase
+			return false; // it is NOT a lowercase string
+		}
 	}
 
 	return true; // for example in cases where \O is entered
--- Section_2/BullCowGame/FBullCowGame.h
+++ Section_2/BullCowGame/FBullCowGame.h
@@ -1,5 +1,13 @@
+/* 
+	The game logic (no view code or direct user interaction)
+	The game is a simple guess the word game based on Mastermind 
+*/
 #pragma once
+
 #include <string>
+#include <vector>
+#include <map>
+#define TMap std::map
 
 using FString = std::string;
 using int32 = int;
@@ -26,6 +34,7 @@ public:
 	int32 GetHiddenWordLength() const;
 
 	bool IsGameWon() const;
+	bool HasMoreWords() const;
 	EGuessStatus IsGuessValid(FString) const;
 
 	void Reset();
@@ -33,10 +42,12 @@ public:
 
 
 private:
+	TMap<int32, FString> HiddenWords;
+	std::vector<int32> RandomNumbers;
 	int32 MyCurrentTry;
-	int32 MyMaxTries;
 	FString MyHiddenWord;
 	bool bGameIsWon;
-
+	bool bMoreWords;
 	bool IsIsogram(FString) const;
+	bool IsLowercase(FString) const;
 };
